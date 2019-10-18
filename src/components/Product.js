e02e9699291f731d59ce5eca044690815c9dd8c6
Edson Moreno <edsonmorenoruiz@gmail.com>
2019-10-18 16:50:23 -0500

    +Delete products, using Axios's delete method

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
 const Product = ({product}) => {
     const deleteProduct = id => {
-        console.log('eliminando: ', id);
+        console.log('Deleting: ', id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then(async(result) => {
+            if (result.value) {
+                try {
+                    const url =`http://localhost:4000/restaurant/${id}`;
+	                const resultado = await axios.delete(url);
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                } catch {
+
+                }
+            }
+        })
     }
    
     return(
